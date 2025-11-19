2e9ca2a5cdd2515e05ff110d76fe2c0d4b6793c0
tesfahunfola <122350783+tesfahunfola@users.noreply.github.com>
2025-11-18 22:21:09 -0800

    Update myAnswer.sql

--- myAnswer.sql
+++ myAnswer.sql
@@ -4,3 +4,34 @@ from Products
 where unitsInStock = 0 and unitsOnOrder >= 1
 order by productName
 ;
+
+
+-- Northwind
+-- Find customers whose company name starts with “A”.
+USE northwind;
+select *
+from customers
+where CompanyName like "A%";
+
+-- List actors with no middle name (if applicable in dataset).
+USE sakila
+SELECT *
+from actor
+where middle_name is NULL;
+
+-- Sakila
+-- Show distinct film ratings.
+use sakila;
+select distinct rating
+from film;
+
+
+-- Show the maximum freight amount from orders.
+USE northwind;
+select max(Freight)
+from orders;
+
+-- Find the average film length.
+use sakila;
+select avg(length)
+from film;
